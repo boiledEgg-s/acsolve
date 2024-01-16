895a4efdf6f3a6806e57c8cce72e5a8561ec232b
Seokjun Lee <seokjun2000@konkuk.ac.kr>
2024-01-16 09:17:24 +0900

    problem_11726 complete

---
+++ src/class3/problem_11726_2nTiling/Main.java
@@ -0,0 +1,25 @@
+package class3.problem_11726_2nTiling;
+
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] result = new int[1001];
+        result[1] = 1;
+        result[2] = 2;
+
+        for(int i=3; i<=n; i++){
+            result[i] = (result[i-1] + result[i-2])%10007;
+        }
+
+        bw.write(result[n]+"\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
